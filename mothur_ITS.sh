d55ac63d41552287da98040ea21061785e98bcdc
Luca Cozzuto <lucacozzuto@gmail.com>
2018-11-09 15:14:16 +0100

    Update mothur_ITS.sh

--- mothur_ITS.sh
+++ mothur_ITS.sh
@@ -17,20 +17,49 @@ summary.seqs(count=stability.trim.contigs.good.count_table, fasta=stability.trim
 
 #Pre-cluster. We used 1% difference to reduce computation time (4 differences in an amplicon ~300/500). You might want to run this step in bach mode since is slow.
 pre.cluster(fasta=stability.trim.contigs.good.unique.fasta, diffs=4, count=stability.trim.contigs.good.count_table)
+summary.seqs(fasta=stability.trim.contigs.good.unique.precluster.fasta, count=stability.trim.contigs.good.unique.precluster.count_table)
 
-#Pre-cluster. We used 1% difference to reduce computation time (4 differences in an amplicon ~300/500). You might want to run this step in bach mode since is slow.
-pre.cluster(fasta=stability.trim.contigs.good.unique.fasta, diffs=4, count=stability.trim.contigs.good.count_table)
-summary.seqs(fasta=current, count=current)
+#Remove rare clusters (i.e. cluster composed by only one contig, likley to be porduced by sequencing errors)
+split.abund(fasta=stability.trim.contigs.good.unique.precluster.fasta, count=stability.trim.contigs.good.unique.precluster.count_table, cutoff=1, accnos=true)
+summary.seqs(fasta=stability.trim.contigs.good.unique.precluster.abund.fasta, count=stability.trim.contigs.good.unique.precluster.abund.count_table) 
+
 
 #Scan for chimeras
-chimera.vsearch(fasta=stability.trim.contigs.good.good.unique.precluster.fasta, count=stability.trim.contigs.good.good.unique.precluster.count_table)
-remove.seqs(fasta=current, accnos=current, count=current)
-summary.seqs(fasta=current, count=current)
+chimera.vsearch(fasta=stability.trim.contigs.good.unique.precluster.abund.fasta, count=stability.trim.contigs.good.unique.precluster.abund.count_table)
+remove.seqs(fasta=stability.trim.contigs.good.unique.precluster.abund.fasta, accnos=stability.trim.contigs.good.unique.precluster.abund.denovo.vsearch.accnos, count=stability.trim.contigs.good.unique.precluster.abund.count_table)
+summary.seqs(fasta=stability.trim.contigs.good.unique.precluster.abund.pick.fasta, count=stability.trim.contigs.good.unique.precluster.abund.pick.count_table)
 
 #Classify your reads according to silva
 #RDP classifier using silva as the reference (similar results as RDP reference but some are classified to species)
-classify.seqs(fasta=current, count=current, reference=UNITEv6_sh_dynamic.fasta, taxonomy=UNITEv6_sh_dynamic.tax, cutoff=60)
+classify.seqs(fasta=stability.trim.contigs.good.unique.precluster.abund.pick.fasta, count=stability.trim.contigs.good.unique.precluster.abund.pick.count_table, reference=UNITEv6_sh_dynamic_s.fasta, taxonomy=UNITEv6_sh_dynamic.tax, cutoff=60, processors=44)
 
+#Remove unwanted lineages (Bacteria-unkknown-Protista)
+remove.lineage(fasta=stability.trim.contigs.good.unique.precluster.abund.pick.fasta, count=stability.trim.contigs.good.unique.precluster.abund.pick.count_table, taxonomy=stability.trim.contigs.good.unique.precluster.abund.pick.UNITEv6_sh_dynamic_s.wang.taxonomy, taxon=Bacteria-unknown-Protista)
+summary.seqs(fasta=stability.trim.contigs.good.unique.precluster.abund.pick.pick.fasta, count=stability.trim.contigs.good.unique.precluster.abund.pick.pick.count_table)
 
+#cluster using vsearch, default level is cutoff=0.03, since these are ITS we'll use 5% instead
+cluster(fasta=stability.trim.contigs.good.unique.precluster.abund.pick.pick.fasta, count=stability.trim.contigs.good.unique.precluster.abund.pick.pick.count_table, method=agc, cutoff=0.05)
+
+#make OTU matrix 
+make.shared(list=stability.trim.contigs.good.unique.precluster.abund.pick.pick.agc.unique_list.list, count=stability.trim.contigs.good.unique.precluster.abund.pick.pick.count_table)
+
+#classify each OTU, used the RDP classification 100% means all seqs in that OTU match at that classification level
+classify.otu(list=stability.trim.contigs.good.unique.precluster.abund.pick.pick.agc.unique_list.list, count=stability.trim.contigs.good.unique.precluster.abund.pick.pick.count_table, taxonomy=stability.trim.contigs.good.unique.precluster.abund.pick.UNITEv6_sh_dynamic_s.wang.pick.taxonomy)
+
+get.oturep(fasta=stability.trim.contigs.good.unique.precluster.abund.pick.pick.agc.unique_list.list, list=current, method=abundance)
+
+#check number of sequences in each sample
+count.groups(shared=stability.trim.contigs.good.unique.precluster.abund.pick.pick.agc.unique_list.shared)
+
+#alpha diversity
+summary.single(shared=stability.trim.contigs.good.unique.precluster.abund.pick.pick.agc.unique_list.share, calc=nseqs-sobs-coverage-shannon-shannoneven-invsimpson, subsample=10000)
+
+#beta diversity
+dist.shared(shared=stability.trim.contigs.good.unique.precluster.abund.pick.pick.agc.unique_list.shared, calc=braycurtis-jest-thetayc, subsample=10000)
+
+#make a rarefied OTU table for heatmaps, indicator species, etc
+sub.sample(shared=stability.trim.contigs.good.unique.precluster.abund.pick.pick.agc.unique_list.shared, size=10000)
+ 
+summary.tax(taxonomy=current, count=current)
 
 get.current()
